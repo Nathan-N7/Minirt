319134a217d0dcf20686d7a3f918680ecd6f4a84
Nathan-N7 <nathan.rod1710@gmail.com>
2025-07-03 17:42:50 -0300

    forgot include the header

--- vector/vector.h
+++ vector/vector.h
@@ -19,5 +19,6 @@ t_vec	vec_scale(t_vec vec1, double scale);
 double	vec_length(t_vec vec);
 t_vec	vec_sub(t_vec a, t_vec b);
 double  vec_prodesc(t_vec a, t_vec b);
+t_vec	vec_normalize(t_vec a);
 
 #endif
